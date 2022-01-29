5471d7bcb0ddf3a9d1a9e9ca8d84aec66da93ac8
JoeyEdmo <97484115+JoeyEdmo@users.noreply.github.com>
2022-01-28 16:57:23 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,6 +18,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if(closeParen == openParen + 1){
+                continue;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
